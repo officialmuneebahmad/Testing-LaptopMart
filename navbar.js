e8b0d278b5a3e5ed79a5bdb357db103459073295
officialmuneebahmad <icsf23m@gmail.com>
2024-09-29 22:06:11 +0500

    First commit

---
+++ navbar.js
@@ -0,0 +1,11 @@
+        // navbar.js
+    fetch('./navbar/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading navbar:', error));
+
+//-----------------------------------------------------------------------
+
+       
